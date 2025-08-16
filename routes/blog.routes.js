2e4f3f63a7f8cfbd1ab18003358487c0d3c45708
Abbatyya <abbaty48@gmail.com>
2025-08-16 15:25:35 +0100

    Implement blog get endpoint business logic.
    Add schema plugins.
    
    Register schema plugin to the application.
    
    Rename get schema to json lieud to js.

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -2,7 +2,43 @@ import fastifyPlugin from "fastify-plugin";
 
 export default fastifyPlugin(async (app) => {
 	app
-		.get("/posts/:id?", async (request, reply) => {})
+		.get(
+			"/posts/:id?",
+			{
+				schema: { $ref: "schema:blog:get" },
+			},
+			async (request, _reply) => {
+				try {
+					const { id } = request.params;
+					const { title, content, category, tags } = request.query;
+
+					const { getConnection, escape, query } = app.mysql;
+					const connection = await getConnection();
+
+					console.log("Q: ", title, content, category, tags);
+					let whereClause = "";
+					title ? (whereClause += `title=${escape(title)}&&`) : "";
+					content ? (whereClause += `content=${escape(content)}&&`) : "";
+					category ? (whereClause += `category=${escape(category)}&&`) : "";
+					tags
+						? (whereClause += `FIND_IN_SET('tags', ${escape(tags)}) > 0`)
+						: "";
+
+					let qs = id
+						? `SELECT * FROM posts WHERE id = ${escape(id)}`
+						: `SELECT * FROM posts WHERE ${whereClause.replace(/&&$/, "")}`;
+
+					const [rows] = await query(qs);
+					connection.release();
+
+					return { rows };
+				} catch (error) {
+					return {
+						message: error?.message || "Opps, I'm sorry, something went wrong.",
+					};
+				}
+			},
+		)
 		.post(
 			"/posts",
 			{ schema: { $ref: "schema:blog:post" } },
@@ -12,19 +48,19 @@ export default fastifyPlugin(async (app) => {
 					const { title, content, category, tags } = request.body;
 					const connection = await app.mysql.getConnection();
 					const [insertId] = await connection.query(
-						`INSERT INTO posts(title, content, category, tags, createdAt, updatedAT) VALUES (?, ?, ?, ?, ?, ?);`,
+						`INSERT INTO posts(title, content, category, tags, createdAt, updatedAT) VALUES(?, ?, ?, ?, ?, ?); `,
 						[title, content, category, tags, now, now],
 					);
 					connection.release();
 					return insertId
 						? {
-								title,
-								content,
-								category,
-								tags: tags.split(","),
-								createdAt: now,
-								updatedAt: now,
-							}
+							title,
+							content,
+							category,
+							tags: tags.split(","),
+							createdAt: now,
+							updatedAt: now,
+						}
 						: false;
 				} catch (error) {
 					return {
@@ -34,6 +70,6 @@ export default fastifyPlugin(async (app) => {
 				}
 			},
 		)
-		.put("/posts/:id", async (request, reply) => {})
-		.delete("/post/:id", async (request, reply) => {});
+		.put("/posts/:id", async (request, reply) => { })
+		.delete("/post/:id", async (request, reply) => { });
 });
