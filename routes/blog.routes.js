27311101d3f18406d5e1b344f7e32d6b5aa4f872
Abbatyya <abbaty48@gmail.com>
2025-08-16 16:31:07 +0100

    Implement blog put endpoint business logic.
    Implement put schema json.
    
    Rename put schema to json lieud to js.

--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -54,13 +54,13 @@ export default fastifyPlugin(async (app) => {
 					connection.release();
 					return insertId
 						? {
-							title,
-							content,
-							category,
-							tags: tags.split(","),
-							createdAt: now,
-							updatedAt: now,
-						}
+								title,
+								content,
+								category,
+								tags: tags.split(","),
+								createdAt: now,
+								updatedAt: now,
+							}
 						: false;
 				} catch (error) {
 					return {
@@ -70,6 +70,38 @@ export default fastifyPlugin(async (app) => {
 				}
 			},
 		)
-		.put("/posts/:id", async (request, reply) => { })
-		.delete("/post/:id", async (request, reply) => { });
+		.put(
+			"/posts/:id",
+			{ schema: { $ref: "schema:blog:put" } },
+			async (request, _reply) => {
+				const { id } = request.params;
+				const { getConnection, query } = app.mysql;
+				const connection = await getConnection();
+				let [rows] = await query(`SELECT * FROM posts WHERE id = ?`, [id]);
+
+				if (rows.length) {
+					const { title, content, category, tags } = Object.assign(
+						rows[0],
+						request.body,
+					);
+					const updatedAt = new Date(Date.now());
+					const [result] = await query(
+						"UPDATE posts SET title =?, content = ?, category = ?, tags = ?, updatedAt = ?",
+						[title, content, category, tags, updatedAt],
+					);
+
+					const { affectedRows, changedRows } = result;
+					await connection.release();
+					return affectedRows && changedRows
+						? { title, content, category, tags, updatedAt }
+						: false;
+				}
+
+				return {
+					message:
+						"Opps, a post with that id does not exist, please check and try again.",
+				};
+			},
+		)
+		.delete("/post/:id", async (request, reply) => {});
 });
